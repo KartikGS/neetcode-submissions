76d13ecf2453e51717e0984e3ed7560165a9f3e0
Kartik Shetty <starkartik9@gmail.com>
2026-07-07 11:23:48 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+        vector<int> step(n);
+        step[0] = 1;
+        step[1] = 2;
+        for(int i = 2;i<n;i++){
+            step[i] = step[i-1] + step[i-2];
+        }
+        return step[n-1];
+    }
+};
