eb5f10cb79aea2e38e21751ff3a6b44cddbbabf8
Kartik Shetty <starkartik9@gmail.com>
2026-07-07 14:41:05 +0530

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> dp;
+
+    int rob(vector<int>& nums) {
+        if(nums.size()==1) return nums[0];
+        if(nums.size()==2) return max(nums[0],nums[1]);
+        
+        vector<int> dp(nums.size());
+        
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        
+        for(int i = 2; i<nums.size(); i++){
+            dp[i] = max(nums[i]+dp[i-2], dp[i-1]);
+        }
+
+        return dp[nums.size()-1];
+    }
+//[5,1,7,11,13,13,20,22,23,23]
+};
