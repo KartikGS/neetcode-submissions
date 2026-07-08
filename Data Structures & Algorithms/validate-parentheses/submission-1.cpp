edf8e2334e834d7ecfa3fe3a4b281c137aa8e190
Kartik Shetty <starkartik9@gmail.com>
2026-07-08 12:02:25 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+		unordered_map<char, char> closeToOpen = {{')','('},{'}','{'},{']','['}};
+
+		for(auto c: s){
+			if(closeToOpen.count(c)){
+				if(!stk.empty() && stk.top() == closeToOpen[c]){
+					stk.pop();
+				} else {
+					return false;
+				}
+			} else {
+				stk.push(c);
+			}
+		}
+
+		return stk.empty();
+    }
+};
