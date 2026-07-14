88cbbac60d8f6d30d170ef98bb3499039fb1ba7f
Kartik Shetty <starkartik9@gmail.com>
2026-07-14 10:10:34 +0530

    Add: longest-increasing-subsequence - submission-18

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-18.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    vector<vector<int>> memo;
+
+    int lengthOfLIS(vector<int>& nums) {
+        memo.resize(nums.size(), vector<int>(nums.size()+1, -1));
+        return dfs(nums, 0, -1);
+    }
+
+    int dfs(vector<int>& nums, int i, int j){
+        if(i == nums.size()) return 0;
+        if(memo[i][j+1]!=-1) return memo[i][j+1];
+
+        int lis = dfs(nums, i+1, j);
+
+        if(j==-1 || nums[j]<nums[i]) lis = max(lis, 1+dfs(nums, i+1, i));
+
+        memo[i][j+1] = lis;
+        return lis;
+    }
+};
