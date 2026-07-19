21408703b9400c08485b1143f4b0ec4d66295b36
Kartik Shetty <starkartik9@gmail.com>
2026-07-19 20:19:55 +0530

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+	vector<vector<bool>> visiting;
+	vector<pair<int, int>> drc = {{1,0}, {-1, 0}, {0,1}, {0,-1}};
+    	bool exist(vector<vector<char>>& board, string word) {
+        		int rows = board.size();
+		int cols = board[0].size();
+
+		visiting.resize(rows, vector<bool>(cols, false));
+
+		for(int r=0; r<rows; r++){
+			for(int c =0; c<cols; c++){
+				if(dfs(board, word, 0, r, c)) return true;
+}
+}
+
+return false;
+    	}
+	
+	bool dfs(vector<vector<char>>& board, string word, int idx, int r, int c){
+		if(idx == word.size()) return true;
+		if( r<0 || r>= board.size() || c<0 || c>=board[0].size() || board[r][c] != word[idx] || visiting[r][c]) return false;
+
+		visiting[r][c] = true;
+		for(pair<int, int> d: drc){
+			if(dfs(board, word, idx+1, r+d.first, c+d.second)) return true;
+}
+visiting[r][c] = false;
+
+return false;
+}
+};
+
+
+
