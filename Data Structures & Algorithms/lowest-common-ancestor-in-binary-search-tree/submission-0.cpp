2c871c7d0dcb35ccf98ed1f0830a59dd2637f655
Kartik Shetty <starkartik9@gmail.com>
2026-07-19 21:49:37 +0530

    Add: lowest-common-ancestor-in-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-0.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+	TreeNode* pg;
+	TreeNode* qg;
+    	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+TreeNode* res;
+pg = p;
+qg = q;
+dfs(root, res);
+return res;        
+    	}
+	pair<bool, bool> dfs(TreeNode* root, TreeNode*& res){
+		if(!root) return {false, false};
+		pair<bool, bool> lst = dfs(root->left, res);
+		pair<bool, bool> rst = dfs(root->right, res);
+		
+		if((lst.first && lst.second) || (rst.first && rst.second)) return {true, true};
+		if((lst.first && rst.second) || (lst.second && rst.first)){
+	res = root;
+	return {true, true};
+}
+if(root == pg) {
+	if(lst.second || rst.second) res = root;
+return {true, lst.second || rst.second};
+}
+if(root == qg) {
+	if(lst.first || rst.first) res = root;
+return {lst.first || rst.first, true};
+}
+
+return {lst.first || rst.first, lst.second || rst.second};
+}
+};
+
+
+
