85b261898c84c9488cdc9f87c6f515daf1b05ca9
Kartik Shetty <starkartik9@gmail.com>
2026-07-22 21:33:19 +0530

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> dp(n, false);
+        dp[n-1] = true;
+
+        for(int i = n-2; i>=0; i--){
+            int j=1;
+            while(j<=nums[i] && i+j<n){
+                if(dp[i+j]){
+                    dp[i] = true;
+                    break;
+                }
+                j++;
+            }
+        }
+
+        return dp[0];
+    }
+};
