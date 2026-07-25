2e7d5869d32377695ca18dbc8a46148c8eeb0cc7
Kartik Shetty <starkartik9@gmail.com>
2026-07-25 17:10:38 +0530

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.cpp
@@ -0,0 +1,47 @@
+class Solution {
+    int numOfBits(int num){
+        int bits = 0;
+        int mx = 1, po =0;
+        while(mx<=num){
+            mx*=2;
+            po++;
+        }
+        po--;
+        while(num>0 && po>=0){
+            if(num>=pow(2,po)){
+                bits++;
+                num-=pow(2, po);
+            }
+            po--;
+        }
+        return bits;
+    }
+
+public:
+    vector<int> countBits(int n) {
+        vector<int> res;
+
+        for(int i = 0; i<=n; i++){
+            res.push_back(numOfBits(i));
+        }
+
+        return res;
+    }
+};
+// 0 --> 0 0
+// 1 --> 1 1
+// 2 --> 10 1
+// 3 --> 11 2
+// 4 --> 100 1
+// 5 --> 101 2
+// 6 --> 110 2
+// 7 --> 111 3
+// 8 --> 1000 1
+// 9 --> 1001 2
+// 10 --> 1010 2
+// 11 --> 1011 3
+// 12 --> 1100 2
+// 13 --> 1101 3
+// 14 --> 1110 3
+// 15 --> 1111 4
+// 16 --> 10000 1
