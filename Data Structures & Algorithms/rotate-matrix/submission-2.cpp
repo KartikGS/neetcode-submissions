3bbff2bc2ad259007d369d934ab557a54caaa599
Kartik Shetty <starkartik9@gmail.com>
2026-08-01 13:16:59 +0530

    Add: rotate-matrix - submission-2

---
+++ Data Structures & Algorithms/rotate-matrix/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int l = 0; 
+		int r = matrix.size()-1;
+
+		while(l<r){
+			int t = l, b =r;
+			for(int i = 0; i<r-l; i++){
+				int tl = matrix[t][l+i];
+				matrix[t][l+i] = matrix[b-i][l];
+				matrix[b-i][l] = matrix[b][r-i];
+				matrix[b][r-i] = matrix[t+i][r];
+				matrix[t+i][r] = tl;
+			}
+			l++;
+			r--;
+		}
+    }
+};
