17eef860d5c0f11ce76f1a622f7cfcf266d97401
Kartik Shetty <starkartik9@gmail.com>
2026-08-01 16:15:03 +0530

    Add: spiral-matrix - submission-3

---
+++ Data Structures & Algorithms/spiral-matrix/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> res;
+		vector<pair<int, int>> drc = {{0,1}, {1,0}, {0,-1}, {-1, 0}};
+		vector<int> steps = {(int)matrix[0].size(), (int)matrix.size()-1};
+
+		int d = 0;
+		int r = 0, c = -1;
+		while(steps[d%2]){
+			for(int i = 0; i<steps[d%2]; i++){
+				r+=drc[d].first;
+				c+=drc[d].second;
+				res.push_back(matrix[r][c]);
+			}
+			steps[d%2]-=1;
+			d = (d+1)%4;
+		}
+
+		return res;
+    }
+};
