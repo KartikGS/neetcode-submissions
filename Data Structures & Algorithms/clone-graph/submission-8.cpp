6bae249666762450ec0442b965204d1d10ce1148
Kartik Shetty <starkartik9@gmail.com>
2026-08-26 21:34:40 +0530

    Add: clone-graph - submission-8

---
+++ Data Structures & Algorithms/clone-graph/submission-8.cpp
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node*, Node*> oldToNew;
+    Node* cloneGraph(Node* node) {
+        if(node == nullptr) return node;
+        return dfs(node);
+    }
+
+    Node* dfs(Node* node){
+        if(oldToNew.count(node)) return oldToNew[node];
+        
+        Node* clone = new Node(node->val);
+        oldToNew[node] = clone;
+
+        for(Node* nei: node->neighbors){
+            clone->neighbors.push_back(dfs(nei));
+        }
+
+        return clone;
+    }
+};
